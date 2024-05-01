ba5f635e682150e9b9996a6f82a2ad736253ebfc
Arul-1911 <arulpraveen19112002@gmail.com>
2024-05-01 20:34:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { chats } = require('./Data/ChatData.js');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+
+app.get('/',(req,res) => {
+   res.send('sample check of api')
+})
+
+app.get('/api/chat', (req,res) => {
+   res.send(chats)
+})
+
+app.get('/api/chat/:id', (req,res) => {
+   // console.log(req.params.id);
+   const singleid = chats.find((e) => e._id === req.params.id);
+   res.send(singleid)
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log(`server running in port: ${PORT}`))
