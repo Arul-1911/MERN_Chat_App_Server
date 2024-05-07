47cf5e648aff581bbf1a8ed38c4c02acffc043b3
Arul-1911 <arulpraveen19112002@gmail.com>
2024-05-07 17:47:37 +0530

    changes in chat controller

--- Controllers/chatControllers.js
+++ Controllers/chatControllers.js
@@ -49,25 +49,25 @@ const accessChat = asyncHandler(async (req, res) => {
 });
 
 //FETCH CHATS FOR PARTICULAR USER
-const fetchChats = asyncHandler(async (req, res) => {
-  try {
-    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
-      .populate("users", "-password")
-      .populate("groupAdmin", "-password")
-      .populate("latestMessage")
-      .sort({ upadedAt: -1 })
-      .then(async (results) => {
-        results = await User.populate(results, {
-          path: "latestMessage.select",
-          select: "userName pic email",
-        });
+  const fetchChats = asyncHandler(async (req, res) => {
+    try {
+      Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password")
+        .populate("latestMessage")
+        .sort({ upadedAt: -1 })
+        .then(async (results) => {
+          results = await User.populate(results, {
+            path: "latestMessage.select",
+            select: "userName pic email",
+          });
 
-        res.status(200).send(results);
-      });
-  } catch (error) {
-    return res.status(400).json({ message: "unable to retrive chat" });
-  }
-});
+          res.status(200).send(results);
+        });
+    } catch (error) {
+      return res.status(400).json({ message: "unable to retrive chat" });
+    }
+  });
 
 //GROUP CHAT ROUTE
 const createGroupChat = asyncHandler(async (req, res) => {
